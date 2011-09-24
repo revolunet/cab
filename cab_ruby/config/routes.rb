e6a5674c4a09b7c9bb28c400f918d4587ea97b46
Lothar <thomas.egret@gmail.com>
2011-09-24 10:16:07 +0200

    commit github

--- cab_ruby/config/routes.rb
+++ cab_ruby/config/routes.rb
@@ -50,7 +50,7 @@ CabRuby::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'user_session#index'
+   root :to => 'user_sessions#index'
 
   # See how all your routes lay out with "rake routes"
 
