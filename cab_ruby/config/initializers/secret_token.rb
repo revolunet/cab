914778c1338ce252c5c6549f5de8dabaa85564f1
Lothar <thomas.egret@gmail.com>
2011-09-24 08:31:03 +0200

    initial commit

---
+++ cab_ruby/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CabRuby::Application.config.secret_token = 'eb0d78bc4536be7143f832b24e58630776aa5de66efbe70ca1beb6ea024e47ce0760d41ac9833a5c6b55358d4a7121fdc3b84e38ba64ed5da0170848e67cf4e4'
